aeb1a5626b15beb61718a4ed710dd3bc7831bdb6
woundmee <woundmee@gmail.com>
2026-01-31 23:53:20 +0700

    two sum - corrected

--- main.go
+++ main.go
@@ -5,15 +5,17 @@ import "fmt"
 // problem -> https://leetcode.com/problems/two-sum/description/
 
 func main() {
-	s := []int{1, 2, 3, 4}
-	fmt.Println(twoSum(s, 5))
+	s := []int{3, 2, 3}
+	fmt.Println(twoSum(s, 6))
 }
 
 // nums = [2, 7, 1, 5], target = 9, out: [0, 1]
 func twoSum(nums []int, target int) []int {
-	for i := 1; i <= len(nums); i++ {
-		if nums[i]+nums[i-1] == target {
-			return []int{i - 1, i}
+	for i := range nums {
+		for j := range i {
+			if nums[j]+nums[i] == target {
+				return []int{j, i}
+			}
 		}
 	}
 	return []int{}
