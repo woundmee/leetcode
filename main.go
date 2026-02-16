698207881e2cfcd6a2934e321df0335d94a66828
woundmee <woundmee@gmail.com>
2026-02-16 11:24:24 +0700

    stack: baseball-game

--- main.go
+++ main.go
@@ -1,48 +1,58 @@
 package main
 
-// problem --> https://neetcode.io/problems/implement-stack-using-queues/
+import (
+	"strconv"
+)
+
+// problem --> https://neetcode.io/problems/baseball-game/
 
 func main() {
-
+	calPoints([]string{"1", "2", "+", "C", "5", "D"})
 }
 
-type MyStack struct {
-	queue []int
-}
+// in: [1 2 + C 5 D]
+// out: 18
 
-func Constructor() MyStack {
-	return MyStack{
-		queue: make([]int, 0),
+// explantation:
+// 1 - add
+// 2 - add   --> [1 2]
+// + - суммирование 2х предыдущих: 1+2  -> [1 2 3]
+// C - удалить предыдущий результат  -> [1 2]
+// 5 - добавление  -> [1 2 5]
+// D - умножение 2х предыдущих: 2*5  -> [1 2 5 10]
+
+// out: сумма всех: 1+2+5+10 = 18
+
+// ================= //
+// 1 2
+
+func calPoints(operations []string) int {
+
+	stack := make([]int, 0, len(operations))
+
+	for i := range operations {
+		if digit, err := strconv.Atoi(operations[i]); err == nil {
+			stack = append(stack, digit)
+			continue
+		}
+
+		switch operations[i] {
+		case "+":
+			// note: нужно условие проверки на len(stack) >= 2 ?!
+			n := len(stack)
+			addition := stack[n-1] + stack[n-2]
+			stack = append(stack, addition)
+		case "C":
+			stack = stack[:len(stack)-1]
+		case "D":
+			stack = append(stack, stack[len(stack)-1]*2)
+		}
 	}
-}
 
-func (this *MyStack) Push(x int) {
-	this.queue = append(this.queue, x)
-	// slices.Reverse(this.queue)  // по условию задачи нельзя использовать
-
-	// нужно перевернуть список
-	// было: [1 2 3]
-	// стало: [3 2 1]
-	// т.е. очередь сделать ровном наоборот
-
-	for range len(this.queue) - 1 {
-		val := this.queue[0]                 // 1-ый элемент
-		this.queue = this.queue[1:]          // удалили 1-ый элемент
-		this.queue = append(this.queue, val) // добавили в конец
-
+	var sum int
+	for i := range stack {
+		sum += stack[i]
 	}
-}
 
-func (this *MyStack) Pop() int {
-	val := this.queue[0]
-	this.queue = this.queue[1:]
-	return val
-}
-
-func (this *MyStack) Top() int {
-	return this.queue[0]
-}
-
-func (this *MyStack) Empty() bool {
-	return len(this.queue) == 0
+	return sum
 }
