018ab85fc763b18cd7dd4226858b770f790178ee
woundmee <woundmee@gmail.com>
2026-02-22 19:42:06 +0700

    2 pointers: apply operations to an array

--- main.go
+++ main.go
@@ -1,24 +1,37 @@
 package main
 
-// problem --> https://neetcode.io/problems/move-zeroes/
+// problem --> https://leetcode.com/problems/apply-operations-to-an-array/
 
 func main() {
-	moveZeroes([]int{0, 0, 1, 2, 0, 5})
-	moveZeroes([]int{1, 0, 2, 0, 0, 3, 4})
-
+	// applyOperations([]int{1, 2, 2, 1, 1, 0})
+	applyOperations([]int{847, 847, 0, 0, 0, 399, 416, 416, 879, 879, 206, 206, 206, 272})
 }
 
-// in:	0,0,1,2,0,5
-// out:	1,2,5,0,0,0
+// in:	[1,2,2,1,1,0]
+// out:	[1,4,2,0,0,0]
 
-// note: задача двигать нули в конец
+// если соседние элементы равны (i == i+1), тогда i умножаем на 2 и записываем
+// вместо него новый результат, а в i+1 записываем 0.
+// в конце просто сдвигаем все нули в конец
 
-func moveZeroes(nums []int) {
-	var left int
+func applyOperations(nums []int) []int {
+
+	// step #1
+	for i := 0; i < len(nums)-1; i++ {
+		if nums[i] == nums[i+1] {
+			nums[i] *= 2
+			nums[i+1] = 0
+		}
+	}
+
+	// step #2
+	left := 0
 	for right := range nums {
 		if nums[right] != 0 {
 			nums[left], nums[right] = nums[right], nums[left]
 			left++
 		}
 	}
+
+	return nums
 }
