416c388c04f9f35d039ee54424a7f4fd0b8a6cf4
woundmee <woundmee@gmail.com>
2026-03-07 15:30:52 +0700

    linked list: cycle detection

--- main.go
+++ main.go
@@ -1,43 +1,36 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// res := findPeakElement([]int{1, 2, 3, 1})
-	// res := findPeakElement([]int{1, 2, 1, 3, 4, 5, 0})
-	// res := findPeakElement([]int{1, 2, 3})
-	// res := findPeakElement([]int{1, 2})
-	res := findPeakElement([]int{3, 2, 1})
-	fmt.Println(res)
+
 }
 
-// problem --> https://neetcode.io/problems/find-peak-element/
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
 
-// in: [1,2,1,3,4,5,0]
-// out: 5 (index)
+// problem --> https://neetcode.io/problems/linked-list-cycle-detection/
 
-// найти пиковый элемент в массиве и вернуть его индекс.
-// пиковый - это когда элемент слева и справа от него меньше него самого
+// Input: head = [1,2,3,4], index = 1
+//          ⮤—————⤶
+// Output: true
 
-func findPeakElement(nums []int) int {
+// голова может начать откуда угодно.
+// нужно испольовать 2 указателя, 1-ый идет на 1 шаг, 2-ой - на 2шага вперед.
+// Проверки:
+//    1. проверить 2-ой на next==nil, тогда вернуть false (цикла нет)
+//    2. и проверить first==second (проверить 2 указателя равны ли друг другу), если цикл есть - они встретятся.
 
-	left, right := 0, len(nums)-1
+func hasCycle(head *ListNode) bool {
+	slow, fast := head, head
 
-	// [1 2 3 1]
-	//    ^
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
 
-	for left < right {
-		mid := left + (right-left)/2
-		if mid > 0 && nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1] {
-			return mid
-		} else if nums[mid] < nums[mid+1] {
-			left = mid + 1
-		} else {
-			right = mid - 1
+		if slow == fast {
+			return true
 		}
 	}
-
-	return left
+	return false
 }
