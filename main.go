063f8216f5d3dedd584be8211e3a6188fe9ca75c
woundmee <woundmee@gmail.com>
2026-03-08 17:18:51 +0700

    linked list: palindrome

--- main.go
+++ main.go
@@ -1,5 +1,8 @@
 package main
 
+// problem --> https://neetcode.io/problems/palindrome-linked-list/
+// проверка на палиндрон односвязного списка
+
 func main() {}
 
 type ListNode struct {
@@ -7,19 +10,73 @@ type ListNode struct {
 	Next *ListNode
 }
 
-// problem —> https://leetcode.com/problems/remove-duplicates-from-sorted-list/
-// удалить все дубликаты в списке
-// in: [1 1 2 2 3 3 4]
-// out: [1 2 3 4]
+// =============================== //
+// ======== RAM = O(n) ========== //
+// =============================== //
+//
+// func isPalindrome(head *ListNode) bool {
+// 	arr := make([]int, 0)
+// 	for head != nil {
+// 		arr = append(arr, head.Val)
+// 		head = head.Next
+// 	}
 
-func deleteDuplicates(head *ListNode) *ListNode {
-	curr := head
-	for curr != nil && curr.Next != nil {
-		if curr.Val == curr.Next.Val {
-			curr.Next = curr.Next.Next
-		} else {
-			curr = curr.Next
-		}
+// 	left, right := 0, len(arr)-1
+
+// 	// [1 2 3 2 1]
+// 	//    ^   ^
+
+// 	for left <= right {
+// 		if arr[left] != arr[right] {
+// 			return false
+// 		}
+// 		left++
+// 		right--
+// 	}
+// 	return true
+// }
+
+// =============================== //
+// ======== RAM = O(1) ========== //
+// =============================== //
+
+func isPalindrome(head *ListNode) bool {
+
+	slow, fast := head, head
+
+	// [1 2 3 2 1]
+
+	// slow будет на середине
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+
 	}
-	return head
+
+	curr := slow
+	var prev *ListNode = nil
+
+	// начиначем с середины и переворачиваем оставшую часть (до конца)
+	for curr != nil {
+		next := curr.Next
+		curr.Next = prev
+		prev = curr
+		curr = next
+	}
+
+	left := head
+	right := prev
+
+	// 2-мя указателям смотрим уже
+	// [1 2 3] [1 2]
+	//  ^       ^
+	for right != nil {
+		if left.Val != right.Val {
+			return false
+		}
+		left = left.Next
+		right = right.Next
+	}
+
+	return true
 }
