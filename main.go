c93c6b4928f53b66fd99c8d58999b1c549a62ba8
woundmee <woundmee@gmail.com>
2026-03-18 14:41:53 +0700

    tree: balanced

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "math"
+
 func main() {}
 
 type TreeNode struct {
@@ -8,46 +10,28 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// problem → https://neetcode.io/problems/binary-tree-diameter
-// задача: определить диаметр дерева
-// диаметр дерева - это определение самой длинное пути между ребер в левом и правом поддереве
-// какой путь самый длинный от значения к значению — это и есть диаметр
+// problem → https://neetcode.io/problems/balanced-binary-tree
+// задача: проверить баланс дерева. Если левая и правая часть сбалансирована — вернуть true.
+// дерево сбалансировано, если разница вычитания дает 0 или 1, а если > 1, то дисбаланс.
 
-// Пример:
-//         1
-//        / \
-//       2   3
-//      / \    \
-//     4   5    6
-//    /         \
-//   7           8
-
-// Корень не в счёт. Самый длинный путь это от 7 до 5 (или наоборот), там 3 ребра.
-
-var maxDiameter int
-
-func diameterOfBinaryTree(root *TreeNode) int {
-
-	if root == nil {
-		return 0
-	}
-
-	// обнуляю, тесты валятся, т.к. состояние переменной сохраняется
-	maxDiameter = 0
-
-	depth(root)
-	return maxDiameter
+func isBalanced(root *TreeNode) bool {
+	return balance(root) != -1
 }
 
-func depth(node *TreeNode) int {
+func balance(node *TreeNode) int {
 	if node == nil {
 		return 0
 	}
 
-	left := depth(node.Left)
-	right := depth(node.Right)
+	left := balance(node.Left)
+	right := balance(node.Right)
 
-	maxDiameter = max(maxDiameter, left+right)
+	if left == -1 || right == -1 {
+		return -1
+	}
 
-	return 1 + max(left, right)
+	if math.Abs(float64(left-right)) > 1 {
+		return -1
+	}
+	return max(left, right) + 1
 }
