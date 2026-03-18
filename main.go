aea8cd03f875aac100946dc4f5a063393ba48cfc
woundmee <woundmee@gmail.com>
2026-03-18 10:41:28 +0700

    tree: diameter

--- main.go
+++ main.go
@@ -8,20 +8,46 @@ type TreeNode struct {
 	Right *TreeNode
 }
 
-// problem → https://neetcode.io/problems/invert-a-binary-tree
-// задача: инвертировать дерево
-// in: [4,2,7,1,3,6,9]    out: [4,7,2,9,6,3,1]
+// problem → https://neetcode.io/problems/binary-tree-diameter
+// задача: определить диаметр дерева
+// диаметр дерева - это определение самой длинное пути между ребер в левом и правом поддереве
+// какой путь самый длинный от значения к значению — это и есть диаметр
 
-func invertTree(root *TreeNode) *TreeNode {
+// Пример:
+//         1
+//        / \
+//       2   3
+//      / \    \
+//     4   5    6
+//    /         \
+//   7           8
+
+// Корень не в счёт. Самый длинный путь это от 7 до 5 (или наоборот), там 3 ребра.
+
+var maxDiameter int
+
+func diameterOfBinaryTree(root *TreeNode) int {
+
 	if root == nil {
-		return nil
+		return 0
 	}
-	// root.Left, root.Right = root.Right, root.Left
-	// invertTree(root.Left)
-	// invertTree(root.Right)
 
-	left := invertTree(root.Left)
-	right := invertTree(root.Right)
-	root.Left, root.Right = right, left
-	return root
+	// обнуляю, тесты валятся, т.к. состояние переменной сохраняется
+	maxDiameter = 0
+
+	depth(root)
+	return maxDiameter
+}
+
+func depth(node *TreeNode) int {
+	if node == nil {
+		return 0
+	}
+
+	left := depth(node.Left)
+	right := depth(node.Right)
+
+	maxDiameter = max(maxDiameter, left+right)
+
+	return 1 + max(left, right)
 }
