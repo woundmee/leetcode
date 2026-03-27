8bd6287d20ea93a0e1550cc956938d6d71e8e81e
woundmee <woundmee@gmail.com>
2026-03-27 22:02:43 +0700

    arrays: longest-consecutive-sequence: O(n log n)...

--- main.go
+++ main.go
@@ -1,45 +1,50 @@
 package main
 
-// problem -> https://neetcode.io/problems/valid-palindrome-ii/
-// задача: проверить текст на палиндром.
-// У тебя есть 1 попытка убрать букву (с левой или правой стороны)
+import (
+	"slices"
+)
 
-// example: "addba" (можно убрать предпоследнюю букву и слово == палиндром)
+// problem -> https://neetcode.io/problems/longest-consecutive-sequence/
+// задача: дан массив, вернуть самую длинную последовательность, которому можно составить в массиве
+// пример:
+// in: [0,3,2,5,4,6,1,1]
+// out: 7
+// максимальная последовательность: 0 1 2 3 4 5 6 (7 раз)
 
 func main() {
-	// validPalindrome("acdccba")  // false
-	validPalindrome("abbda") // true
-	// validPalindrome("aguokepatgbnvfqmgmlcupuufxoohdfpgjdmysgvhmvffcnqxjjxqncffvmhvgsymdjgpfdhooxfuupuculmgmqfvnbgtapekouga")  // true
+	longestConsecutive([]int{2, 20, 4, 10, 3, 4, 5})
+	// longestConsecutive([]int{0, 3, 2, 5, 4, 6, 1, 1})
+	// longestConsecutive([]int{2, 20, 4, 10, 3, 4, 5})
+	longestConsecutive([]int{9, 1, 4, 7, 3, -1, 0, 5, 8, -1, 6})
 }
 
-func validPalindrome(s string) bool {
-
-	left := 0
-	right := len(s) - 1
-
-	for left < right {
-		if s[left] != s[right] {
-
-			l := ispal(s, left+1, right)
-			r := ispal(s, left, right-1)
-			return l || r
-
-		} else {
-			left++
-			right--
-		}
+func longestConsecutive(nums []int) int {
+	if len(nums) < 1 {
+		return 0
 	}
-	return true
-}
 
-func ispal(s string, left, right int) bool {
-	for left < right {
-		if s[left] != s[right] {
-			return false
-		} else {
-			left++
-			right--
+	slices.Sort(nums)
+
+	// 9,1,4,7,3,-1,0,5,8,-1,6
+	// -1 -1 0 1 3 4 5 6 7 8 9
+	//                       ^
+	// count=9
+
+	var count int = 1
+	counts := []int{}
+
+	for i := 1; i <= len(nums)-1; i++ {
+		if nums[i] == nums[i-1] {
+			continue
+		} else if nums[i]-1 != nums[i-1] {
+			counts = append(counts, count)
+			count = 0
 		}
+		count++
 	}
-	return true
+
+	counts = append(counts, count)
+
+	println(slices.Max(counts))
+	return slices.Max(counts)
 }
