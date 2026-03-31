44cdae0d8ec7e46a9785d976183a4f2e68bd4687
woundmee <woundmee@gmail.com>
2026-03-31 16:44:09 +0700

    arrays: sort an array (used quick sort)

--- main.go
+++ main.go
@@ -1,91 +1,42 @@
 package main
 
-import "fmt"
-
-// problem -> https://neetcode.io/problems/design-hashmap/
-// задача: реализовать кастомные методы для своего хеш-мапы
+// problem -> https://neetcode.io/problems/sort-an-array/
+// задача: отсортировать заданный массив. Использовал метод QuickSort
 
 func main() {
-
-	myHashMap := MyHashMap{
-		dict: [][]int{},
-	}
-
-	myHashMap.Put(3, 4)
-	myHashMap.Put(5, 3)
-	myHashMap.Put(2, 1)
-	myHashMap.Put(5, 19)
-
-	fmt.Println(myHashMap)
-
-	// myHashMap.Put(1, 1) // The map is now [[1,1]]
-	// myHashMap.Put(2, 2) // The map is now [[1,1], [2,2]]
-	// myHashMap.Get(1)    // return 1, The map is now [[1,1], [2,2]]
-	// myHashMap.Get(3)    // return -1 (i.e., not found), The map is now [[1,1], [2,2]]
-	// myHashMap.Put(2, 1) // The map is now [[1,1], [2,1]] (i.e., update the existing value)
-	// myHashMap.Get(2)    // return 1, The map is now [[1,1], [2,1]]
-	// myHashMap.Remove(2) // remove the mapping for 2, The map is now [[1,1]]
-	// myHashMap.Get(2)    // return -1 (i.e., not found), The map is now [[1,1]]
-
-	// fmt.Println("myHashMap =", myHashMap)
-
+	sortArray([]int{6, 2, 4, 1, 3, 5})
 }
 
-type MyHashMap struct {
-	dict [][]int // note: можно было юзать просто map[int]int
+func sortArray(nums []int) []int {
+	quickSort(nums, 0, len(nums)-1)
+	return nums
 }
 
-func Constructor() MyHashMap {
-	return MyHashMap{
-		dict: [][]int{},
-	}
-}
+func quickSort(nums []int, left, right int) {
 
-func (this *MyHashMap) Put(key int, value int) {
-
-	// 1. добавление в слайс
-	// 2. изначально пустой слайс
-	// 3. если ключ уже есть в слайсе, обновить его
-	// 4. если ключа нет - добавить в слайс
-
-	for i := range this.dict {
-		if this.dict[i][0] == key {
-			this.dict[i] = []int{key, value}
-			return
-		}
-	}
-
-	// ключа нет - добавляем
-	this.dict = append(this.dict, []int{key, value})
-
-}
-
-func (this *MyHashMap) Get(key int) int { // ok
-
-	for i := range this.dict {
-		if key == this.dict[i][0] {
-			return this.dict[i][1]
-		}
-	}
-
-	return -1
-}
-
-// note: можно было просто вызвать delete()
-func (this *MyHashMap) Remove(key int) { // ok
-	// key=5
-	// [ [1,2], [3,4], [5,2], [4,1] ]
-	//                   ⤷ remove
-
-	if len(this.dict) < 1 {
+	if left >= right {
 		return
 	}
 
-	for i := range this.dict {
-		if this.dict[i][0] == key {
-			this.dict = append(this.dict[:i], this.dict[i+1:]...) // удалил подмассив
-			return
+	pivot := nums[(left+right)/2]
+
+	i, j := left, right
+
+	for i <= j {
+		for pivot > nums[i] {
+			i++
+		}
+		for pivot < nums[j] {
+			j--
+		}
+		if i <= j {
+			nums[i], nums[j] = nums[j], nums[i]
+			i++
+			j--
 		}
 	}
+
+	quickSort(nums, left, j)
+	quickSort(nums, i, right)
 
 }
