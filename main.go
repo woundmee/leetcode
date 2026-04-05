1185c8051ccaf062ceb61e9a5249ebf485694838
woundmee <woundmee@gmail.com>
2026-04-05 23:21:19 +0700

    2 pointer: two sum 2

--- main.go
+++ main.go
@@ -2,43 +2,37 @@ package main
 
 import "fmt"
 
-// problem:	https://neetcode.io/problems/rotate-array/
+// problem:
 // level:	medium
 
-// task: 	дан массив, необходимо его перевернуть k-раз. Если к > len(nums), то k %= len(nums)
-// 			Input: nums = [1,2,3,4,5,6,7,8], k = 4
-// 			Output: [5,6,7,8,1,2,3,4]
+// task:	дается отсортированный массив и K-тый элемент.
+// 			Вернуть массив индексов, числа которых в сумме дают К. Индексы вернуться, начиная с 1, а не 0.
 
-// 			Пояснение:
-// 			поворот на 1 шаг вправо: [8,1,2,3,4,5,6,7]
-// 			поворот на 2 шага вправо: [7,8,1,2,3,4,5,6]
-// 			поворот на 3 шага вправо: [6,7,8,1,2,3,4,5]
-// 			поворот на 4 шага вправо: [5,6,7,8,1,2,3,4]
+//			[2 6 7 8 10 40 50], target=15
+//			out: [3 4] (3-ий индекс == 7, 4-ый == 8), 7+8=15
 
 func main() {
-	// rotate([]int{1, 2, 3, 4, 5, 6, 7, 8}, 4)
-	rotate([]int{1, 2, 3, 4, 5}, 7)
+	twoSum([]int{3, 6, 10, 12, 15, 17, 20}, 25)
+	// twoSum([]int{1, 2, 3, 4}, 3)
 }
 
-// это решение является более оптимальным,
-// чем просто написать: nums = append(nums[l:], nums[:l]...)
-// решение в одну строку, правда создается новый массив, а решение ниже оптимально тем,
-// что меняю исходный на месте (in-place), не создавая новый массив (как при append)
+func twoSum(numbers []int, target int) []int {
 
-func rotate(nums []int, k int) {
-	if len(nums) <= k {
-		k %= len(nums)
-	}
-	reverse(nums, 0, len(nums)-1)
-	reverse(nums, 0, k-1)
-	reverse(nums, k, len(nums)-1)
-	fmt.Println(nums)
-}
+	left, right := 0, len(numbers)-1
 
-func reverse(nums []int, l, r int) {
-	for l <= r {
-		nums[l], nums[r] = nums[r], nums[l]
-		l++
-		r--
+	for left < right {
+		sum := numbers[left] + numbers[right]
+
+		switch {
+		case sum == target:
+			fmt.Println([]int{left + 1, right + 1})
+			return []int{left + 1, right + 1}
+		case sum > target:
+			right--
+		case sum < target:
+			left++
+		}
 	}
+
+	return []int{}
 }
